554f14ee5d5e4cceaa394986b0d1348ea7426139
devemanuelpereira <v-emanuel-pereira@apexgroup.com>
2026-01-29 14:04:19 -0300

    feat: novas respostas HTTP

--- src/apex_development/http/response.py
+++ src/apex_development/http/response.py
@@ -34,12 +34,52 @@ class Response:
         return ResponseEntity(201, body, headers)
 
     @staticmethod
-    def bad_request(body: Any=None):
-        return ResponseEntity(400, body)
+    def accepted(body:Any=None, headers: Any=None):
+        return ResponseEntity(202, body, headers)
+
+    @staticmethod
+    def no_content(headers:Any=None):
+        return ResponseEntity(204, None, headers)
+
+    @staticmethod
+    def bad_request(body:Any=None, headers:Any=None):
+        return ResponseEntity(400, body, headers)
+
+    @staticmethod
+    def unathorized(body:Any=None, headers:Any=None):
+        return ResponseEntity(401, body, headers)
+
+    @staticmethod
+    def forbidden(body:Any=None, headers:Any=None):
+        return ResponseEntity(403, body, headers)
+
+    @staticmethod
+    def not_found(body:Any=None, headers:Any=None):
+        return ResponseEntity(404, body, headers)
+
+    @staticmethod
+    def method_not_allowed(body:Any=None, headers:Any=None):
+        return ResponseEntity(405, body, headers)
+
+    @staticmethod
+    def conflict(body:Any=None, headers:Any=None):
+        return ResponseEntity(409, body, headers)
+
+    @staticmethod
+    def unprocessable_entity(body:Any=None, headers:Any=None):
+        return ResponseEntity(422, body, headers)
 
     @staticmethod
     def failed_request(body:Any, status_code:int):
         return ResponseEntity(status_code, body)
+
+    @staticmethod
+    def internal_server_error(body:Any=None, headers:Any=None):
+        return ResponseEntity(500, body, headers)
+
+    @staticmethod
+    def service_unavailable(body:Any=None, headers:Any=None):
+        return ResponseEntity(503, body, headers)
 
     @staticmethod
     def base64_file(body: bytes, headers: Any=None):
